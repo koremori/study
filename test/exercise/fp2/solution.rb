e3be83692b9c3b74aed023cf1030aa23fd40e659
koremori <ifyourelookingforhotwater@gmail.com>
2021-11-24 02:12:47 +0300

    Complete fp2 exercise

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,43 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each(&block)
+        execute_block(self, block)
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map(&block)
+        my_reduce(MyArray.new) { |acc, item| acc << block.call(item) }
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        my_reduce(MyArray.new) { |acc, item| item.nil? ? acc : acc << item }
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil, &block)
+        diminish(self, acc, block)
+      end
+
+      private
+
+      def execute_block(collection, block)
+        return if collection.empty?
+
+        first, *rest = collection
+        block.call(first)
+        execute_block(rest, block)
+      end
+
+      def diminish(collection, acc, block)
+        return acc if collection.empty?
+
+        first, *rest = collection
+        acc = acc.nil? ? first : block.call(acc, first)
+        diminish(rest, acc, block)
+      end
     end
   end
 end
